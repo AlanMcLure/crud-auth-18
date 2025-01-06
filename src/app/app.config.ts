32ede11287c21d7b9f5ea33403aaea0fde980570
AlanMcLure <alanmclurealarcon02@gmail.com>
2025-01-06 23:02:18 +0100

    Auth + guards hecho

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"crud-auth-18","appId":"1:511439928212:web:52b54f8a3de4e73b366d81","storageBucket":"crud-auth-18.firebasestorage.app","apiKey":"AIzaSyCdpSfSBssfKya4cGG6HSrdquSrbRwiqtA","authDomain":"crud-auth-18.firebaseapp.com","messagingSenderId":"511439928212"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
